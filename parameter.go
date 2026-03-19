350023aefbf842e43e3dd7f2795d602c378e35b3
paulohenriquevn <paulohenriquevn@gmail.com>
2026-03-19 20:12:39 -0300

    refactor: remove unused code and consolidate build helpers into a single file

--- parameter.go
+++ parameter.go
@@ -86,3 +86,94 @@ func (p Parameter) AsOutput() (model.ParameterModel, error) {
 		GlobalName: p.GlobalName,
 	}, nil
 }
+
+// --- Retry ---
+
+// RetryStrategy configures retry behavior for templates.
+type RetryStrategy struct {
+	Limit       *int
+	RetryPolicy RetryPolicy
+	Backoff     *Backoff
+	Expression  string
+}
+
+// Build converts RetryStrategy to its serializable model.
+func (r RetryStrategy) Build() model.RetryStrategyModel {
+	var limit interface{}
+	if r.Limit != nil {
+		limit = fmt.Sprintf("%d", *r.Limit)
+	}
+	var backoff *model.Backoff
+	if r.Backoff != nil {
+		b := *r.Backoff
+		// Normalize factor to string for Argo compatibility
+		if factor, ok := b.Factor.(*int); ok && factor != nil {
+			b.Factor = fmt.Sprintf("%d", *factor)
+		} else if factor, ok := b.Factor.(int); ok {
+			b.Factor = fmt.Sprintf("%d", factor)
+		}
+		backoff = &b
+	}
+	return model.RetryStrategyModel{
+		Limit:       limit,
+		RetryPolicy: string(r.RetryPolicy),
+		Backoff:     backoff,
+		Expression:  r.Expression,
+	}
+}
+
+// --- User Container ---
+
+// UserContainer represents a sidecar or init container in a template.
+type UserContainer struct {
+	// Name is the container name.
+	Name string
+	// Image is the Docker image.
+	Image string
+	// Command is the entrypoint.
+	Command []string
+	// Args are the command arguments.
+	Args []string
+	// WorkingDir is the working directory.
+	WorkingDir string
+	// ImagePullPolicy defines when to pull the image.
+	ImagePullPolicy ImagePullPolicy
+	// Env is the list of environment variables.
+	Env []EnvBuilder
+	// Resources defines CPU/memory.
+	Resources *ResourceRequirements
+	// VolumeMounts are the volume mounts.
+	VolumeMounts []VolumeBuilder
+	// Ports exposed by the container.
+	Ports []ContainerPort
+	// Mirror enables mirroring volume mounts from the main container.
+	Mirror *bool
+	// SecurityContext for the container.
+	SecurityContext *model.SecurityContext
+	// Lifecycle defines actions for container lifecycle events.
+	Lifecycle *model.Lifecycle
+	// ReadinessProbe for the container.
+	ReadinessProbe *model.Probe
+	// Daemon marks this sidecar as a daemon.
+	Daemon *bool
+}
+
+// Build creates the serializable ContainerModel.
+func (uc *UserContainer) Build() model.ContainerModel {
+	return model.ContainerModel{
+		Name:            uc.Name,
+		Image:           uc.Image,
+		Command:         uc.Command,
+		Args:            uc.Args,
+		WorkingDir:      uc.WorkingDir,
+		ImagePullPolicy: string(uc.ImagePullPolicy),
+		Env:             buildEnvVars(uc.Env),
+		Resources:       uc.Resources,
+		VolumeMounts:    buildVolumeMountModels(uc.VolumeMounts),
+		Ports:           uc.Ports,
+		SecurityContext: uc.SecurityContext,
+		Mirror:          uc.Mirror,
+		Lifecycle:       uc.Lifecycle,
+		ReadinessProbe:  uc.ReadinessProbe,
+	}
+}
--- template.go
+++
@@ -1,9 +0,0 @@
-package forge
-
-import "github.com/usetheo/theo/forge/model"
-
-// Templatable is implemented by types that can build an Argo Template.
-type Templatable interface {
-	BuildTemplate() (model.TemplateModel, error)
-	GetName() string
-}
